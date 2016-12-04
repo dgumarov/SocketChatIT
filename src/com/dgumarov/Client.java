becd8e58b655b823c9ef952db13f0278b5a950f7
Dinar <dgumarov@gmail.com>
2016-12-04 22:06:24 +0300

    refactored code

--- src/com/dgumarov/Client.java
+++ src/com/dgumarov/Client.java
@@ -2,15 +2,9 @@ package com.dgumarov;
 
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.time.LocalDate;
 import java.util.Scanner;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Created by user on 03.12.16.
- */
 public class Client {
     static Logger logger = Logger.getLogger(Client.class.getName());
     private static Socket socket;
@@ -77,25 +71,6 @@ public class Client {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-
-            /*try (//Socket socket = new Socket("localhost", 8888);
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            ) {
-                writer.write(String.valueOf(LocalDate.now()));
-                writer.newLine();
-                writer.flush();
-                String line = reader.readLine();
-
-                logger.log(Level.INFO, "Line received: {0}", line);
-
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
         }
     }
 
--- src/com/dgumarov/Server.java
+++ src/com/dgumarov/Server.java
@@ -7,14 +7,9 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Created by user on 03.12.16.
- */
 public class Server {
 
     static Logger logger = Logger.getLogger(Server.class.getName());
-    private static List<Socket> clientList = new Vector<>();
-    private static List<String> clientNames = new Vector<>();
     private static Map<String, Socket> clientsMap = new Hashtable<>();
 
     public static void main(String[] args) {
@@ -32,79 +27,44 @@ public class Server {
     static class Worker extends Thread {
         Socket socket;
         String username;
+        private BufferedReader reader;
+        private BufferedWriter writer;
 
         public Worker(Socket socket) {
             this.socket = socket;
-            logger.info("connected clients count: " + clientList.size());
+
+            try
+            {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            }
+            catch (IOException io)
+            {}
         }
 
         @Override
         public void run() {
 
-            try
-            {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                writer.write("Enter your name");
-                writer.newLine();
-                writer.flush();
+            authenticate();
 
-                while (true)
-                {
-                    username = reader.readLine();
-
-                    if (clientNames.contains(username)) {
-                        writer.write("User with such name already registered at the moment. Please enter another name");
-                        writer.newLine();
-                        writer.flush();
-                    }
-
-                    else
-                    {
-                        clientList.add(socket);
-                        break;
-                    }
-
-                }
-
-                clientsMap.put(username, socket);
-
-            }
-            catch (Exception e)
-            {}
-
-
-            try (
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            ) {
+            try {
 
                 String line;
                 while ((line = reader.readLine()) != null) {
                     logger.log(Level.INFO, "Line received: {0}", line);
-                    if (line.equals("exit"))
+                    if (line.equals("exit")) {
+                        writer.write("Goodbye, " + username);
                         break;
-
-                    for (int i = 0; i<clientList.size(); i++)
-                    {
-                        try
-                        {
-                            BufferedWriter currentWriter = new BufferedWriter(new OutputStreamWriter(clientList.get(i).getOutputStream()));
-                            currentWriter.write(username + ": " + line);
-                            currentWriter.newLine();
-                            currentWriter.flush();
-                        }
-                        catch (Exception e)
-                        {}
                     }
-                }
 
+                    writeToAllStreams(username + ": " + line);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                logger.info("client disconnected");
-                clientList.remove(socket);
-                logger.info("connected clients count: " + clientList.size());
+                clientsMap.remove(username);
+                writeToAllStreams("User '" + username + "' has left the chat.");
+                logger.info("Client disconnected. Connected clients count: " + clientsMap.size());
                 if (socket != null) {
                     try {
                         socket.close();
@@ -112,6 +72,64 @@ public class Server {
                         e.printStackTrace();
                     }
                 }
+            }
+        }
+
+
+        private void authenticate()
+        {
+            try
+            {
+                writeToCurrentStream("Enter your name");
+
+                while (true)
+                {
+                    username = reader.readLine();
+
+                    if (clientsMap.containsKey(username)) {
+                        writeToCurrentStream("User with such name already registered at the moment. Please enter another name");
+                    }
+
+                    else
+                    {
+                        clientsMap.put(username, socket);
+                        logger.info("connected clients count: " + clientsMap.size());
+                        writeToCurrentStream("Welcome to chat " + username);
+                        break;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {}
+        }
+
+        private void writeToCurrentStream(String message)
+        {
+            try {
+                writer.write(message);
+                writer.newLine();
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void writeToAllStreams(String message)
+        {
+            Iterator<String> iterator = clientsMap.keySet().iterator();
+
+            while (iterator.hasNext())
+            {
+                try
+                {
+                    BufferedWriter currentWriter = new BufferedWriter(new OutputStreamWriter(clientsMap.get(iterator.next()).getOutputStream()));
+                    currentWriter.write(message);
+                    currentWriter.newLine();
+                    currentWriter.flush();
+                }
+                catch (Exception e)
+                {}
             }
         }
     }
